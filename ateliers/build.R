16d571d68e93284d41d5e3ee0330aed330bf792c
Lino Galiana <linogaliana@yahoo.fr>
2021-06-07 16:23:36 +0200

    corrige liens relatifs + script R

--- ateliers/build.R
+++ ateliers/build.R
@@ -1,4 +1,8 @@
+file.copy("insert-logo.html", "ateliers/insert-logo.html")
+
 setwd("./ateliers")
 
 rmd <- list.files(pattern = ".Rmd")
 lapply(rmd, rmarkdown::render)
+
+lapply(rmd[rmd != "index.Rmd"], knitr::purl)
