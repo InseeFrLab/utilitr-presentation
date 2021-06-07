4c3697b4b81fe9a54460f93a6a55d127175aaa19
Lino Galiana <linogaliana@yahoo.fr>
2021-06-07 15:37:22 +0200

    le dossier n'existaity pas

--- ateliers/build.R
+++ ateliers/build.R
@@ -2,7 +2,3 @@ setwd("./ateliers")
 
 rmd <- list.files(pattern = ".Rmd")
 lapply(rmd, rmarkdown::render)
-
-
-out_html <- list.files(pattern = ".html")
-lapply(out_html, function(i) file.copy(i, to = paste0("../atelier_out/", i)))
