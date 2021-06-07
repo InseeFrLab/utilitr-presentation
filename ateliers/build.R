e30734d101ff02ffa2f71d95bfb4d70bba874ba7
Lino Galiana <linogaliana@yahoo.fr>
2021-06-07 21:44:54 +0200

    retire import fichiers du pipeline

--- ateliers/build.R
+++ ateliers/build.R
@@ -5,6 +5,7 @@ file.copy(here::here('resources'), here::here('ateliers'), recursive = TRUE)
 setwd("./ateliers")
 
 rmd <- list.files(pattern = ".Rmd")
+rmd <- rmd[rmd != "import_fichiers.Rmd"]
 lapply(rmd, rmarkdown::render)
 
 lapply(rmd[rmd != "index.Rmd"], knitr::purl)
