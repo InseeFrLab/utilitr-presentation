3d532ddf067276897e904934e6e9407789c69265
Lino Galiana <linogaliana@yahoo.fr>
2021-06-11 18:44:09 +0200

    archives présentations

--- build/build.R
+++ build/build.R
@@ -1,12 +1,25 @@
 # dir.create("public")
-dir.create("public/archives")
-dir.create("public/ateliers")
 
-lapply(c("public","ateliers","archives", "public/archives", "public/ateliers"), function(i){
+setwd(here::here())
+
+# CREATION INDENTATION -----------------------
+
+dir.create("public/archives")
+dir.create("public/matinees")
+dir.create("public/ateliers")
+dir.create("public/archives/matinees2021")
+
+lapply(c("public",
+         "archives", "public/archives",
+         "ateliers", "public/ateliers",
+         "archives/matinees2021", "public/archives/matinees2021"), function(i){
   file.copy(here::here('resources'), here::here(i), recursive = TRUE)
   file.copy(here::here('css'), here::here(i), recursive = TRUE)
   file.copy(here::here('insert-logo.html'), here::here(i), recursive = TRUE)
 })
+
+
+# LATEST A LA RACINE ------------------
 
 
 lapply(list.files(pattern = "*.Rmd", recursive = FALSE), function(flname){
@@ -16,7 +29,12 @@ lapply(list.files(pattern = "*.Rmd", recursive = FALSE), function(flname){
   file.remove(flname_html)
 })
 
+# ARCHIVES ------------------
+
+
 setwd(here::here("archives"))
+
+## ROOT =====
 
 lapply(list.files(pattern = "*.Rmd", recursive = FALSE), function(flname){
   rmarkdown::render(flname)
@@ -27,6 +45,22 @@ lapply(list.files(pattern = "*.Rmd", recursive = FALSE), function(flname){
   file.remove(flname_html)
 })
 
+## MATINEES =====
+
+setwd(here::here("archives", "matinees2021"))
+
+
+lapply(list.files(pattern = "*.Rmd", recursive = FALSE), function(flname){
+  rmarkdown::render(flname)
+  flname_html <- gsub(".Rmd", ".html", flname)
+  file.copy(gsub(".Rmd", ".html", flname),
+            paste0(here::here("public","archives", flname_html))
+  )
+  file.remove(flname_html)
+})
+
+
+# ATELIERS ------------------
 
 setwd(here::here())
 
